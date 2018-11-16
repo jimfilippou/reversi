8842ec9dcd099f5f742dc3a1390a1d0bf53c7c82
Jim <jimfilippou8@gmail.com>
2018-11-16 15:35:22 +0200

    think i fixed it
    Signed-off-by: Jim <jimfilippou8@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -19,17 +19,21 @@ def terminal_node(b, player):
     return True
 
 
+def map_to_tile(ply):
+    return 'O' if ply == 'ai' else 'X'
+
+
 def snapshot_board(b, player):
     total = 0
     for y in range(8):
         for x in range(8):
-            if b[y][x] == player:
+            if b[y][x] == map_to_tile(player):
                 if (x == 0 or x == 7) and (y == 0 or y == 7):
-                    total += 4  # corner
+                    total += 4  # corner: woo!
                 elif (x == 0 or x == 7) or (y == 0 or y == 7):
-                    total += 2  # side
+                    total += 2  # side: cool!
                 else:
-                    total += 1
+                    total += 1  # Anywhere else: meh...
     return total
 
 
@@ -86,7 +90,7 @@ def best_move(b, player):
         for y in range(8):
             if valid_move(b, x, y, player):
                 try:
-                    move = pick_pos(x, y, copy.deepcopy(b), player)
+                    pick_pos(x, y, copy.deepcopy(b), player)
                     pts = mini_max_a_b(b, player, depth, min_eval, max_eval, True)
                     if pts > max_points:
                         max_points = pts
